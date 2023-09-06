b3a78887b8fcbc0aa69da210ec5eeb1f65641565
firesz25 <olek.cz@outlok.com>
2023-09-06 12:21:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# animls-in-plants
---
+++ entities/entities.py
@@ -0,0 +1,39 @@
+class Entities:
+    _id: int
+    _entity: str
+    _strength: int
+    _iniciation: int
+
+    def __init__(self, entity, strength, iniciation):
+        self._id = id()
+        self._entity = entity
+        self._strength = strength
+        self._iniciation = iniciation
+
+
+class Weed(Entities):
+    _weedid: int
+
+    def __init__(self, entity, strength, iniciation, weedid):
+        super().__init__(entity, strength, iniciation)
+        self._weedid = weedid
+
+
+class Animal(Entities):
+    _animalid: int
+
+    def __init__(self, entity, strength, iniciation, animalid):
+        super().__init__(entity, strength, iniciation)
+        self._animalid = animalid
+
+
+wolf = Animal("wolf", 9, 5, 1)
+sheep = Animal("sheep", 2, 4, 2)
+fox = Animal("fox", 3, 7, 3)
+snake = Animal("snake", 6, 3, 4)
+horse = Animal("Horse", 5, 5, 5)
+hamster = Animal("Hamster", 1, 2, 6)
+
+weed = Weed("weed", 0, 0, 1)
+cocaine = Weed("cocaine", 0, 0, 2)
+Belladonna = Weed("Belladonna", 0, 0, 3)
